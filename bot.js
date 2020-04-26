81c76196adfba0cde1be91c4e678345e5535bec0
Gabby <ataxdude@gmail.com>
2020-04-26 01:42:41 -0500

    [Big Update] !TKCommands * !roll
    +Added a !roll command for dice!
    +Added commands for a few important people!
    +Added a help command, so you can understand what commands there are!
    +Added a *lot* of easter eggs!
    +Plus much more!

--- bot.js
+++ bot.js
@@ -1,15 +1,11 @@
 const {Client, RichEmbed, Attachment } = require('discord.js');
 const fs = require('fs');
-const chalk = require('chalk');
-//const functionList ;
-console.clear();
+const chalk = require('chalk');console.clear();
 console.log(chalk.red('Starting here'));
 const token = fs.readFileSync('DiscordToken.txt', 'utf8');
 
 // Create an instance of a Discord client.
 const client = new Client();
-const status = '';
-
 
 /**START**/
 client.on('ready', () => {
@@ -35,15 +31,15 @@ client.on('ready', () => {
 			});
 		});
 		//sends a message to the log that in 30 minutes a ping test will occur this also works as a timer, that's called every time pingFunction is called
-		setTimeout(function(){ console.log(chalk.green('Next Ping will occur in 30min.'));},1800000);
+		//setTimeout(function(){ console.log(chalk.green('Next Ping will occur in 30min.'));},1800000);
 	}
 	//calls the ping function
 	pingFunction();
 	var bootFinish = Date.now(); //makes another timestamp
 	var bootTime = (bootFinish - bootStart); //subtracts the ms values of the two vars
-	client.user.setPresence({ game: { name: 'waking up...' }, status: 'online' }) //sets the default 'awake' status
+	client.user.setPresence({game: {name: 'waking up...'}, status: 'online'}).then(r =>console.log(chalk.green("Presence Established"))) // sets the default 'awake' status
 	console.log('Start-up time was...' + bootTime + 'ms'); //reports the time it took to boot the bot in ms
-	setInterval(pingFunction,3600000); //sets up a repeat call of the ping function, that happens every hour
+	//setInterval(pingFunction,3600000); //sets up a repeat call of the ping function, that happens every hour
 });
 
 client.login(token); //the login info...
@@ -56,6 +52,8 @@ client.on('message', msg => {
 			msg.channel.send("OUTSTANDING GIRL");
 		}else if(person == "aevery"){
 			msg.channel.send("OUTSTANDING GIRL");
+		}else if(person == "ember"){
+			msg.channel.send("https://cdn.discordapp.com/attachments/572675846679166977/702064325019303946/dont_worry_about_it.png");
 		}
 	}
 	var sameCooldown = false;
@@ -64,23 +62,28 @@ client.on('message', msg => {
 	}else if(msg.content === '!avatar') { //sends the user's avatar back to them
 		// Send the user's avatar URL
 		msg.reply(msg.author.avatarURL);
-	}else if (msg.content === '!info') { //sends a Embed of the bot's info.
+	}else if (msg.content === '!TKinfo') { //sends a Embed of the bot's info.
 		const embed = new RichEmbed()
 		// Set the title of the field
 		.setTitle('Who is Thronekeeper?')
 		// Set the color of the embed
 		.setColor(0x29a329)
 		// Set the main content of the embed
-		.setDescription(`I am Thronekeeper. I am a moderation bot coded in JS, and with the goal of being able to accurately moderate, and control the workings of a server.\n This bot is in early development.\n Version: Beta-1.1`);
+		.setDescription(`I am Thronekeeper. I am a moderation bot coded in JS, and with the goal of being able to accurately moderate, and control the workings of a server.\n 
+		This bot is in early development.\n
+		Primary feature is supposed to be bulk user deletion. \n
+		Version: Beta-1.3`);
 		// Send the embed to the same channel as the message
 		msg.channel.send(embed);
+
 	}else if (msg.content.startsWith('!admin')){ //admin-only commands
+
 		if (msg.author.id !== '181187505448681472'){ //verifies that Contrastellar#0001 is the only user to do this.
 			msg.channel.send('You do not have sufficent perms. https://i.kym-cdn.com/entries/icons/mobile/000/028/925/Screen_Shot_2019-03-15_at_11.01.54_AM.jpg'); //clowns on a fool
-			console.log('invalid status-change attempt from' + msg.author + ' ' + msg.author.username);
-		} else{
+
+		} else {
 			console.log(chalk.yellow('<REPORT> ')+ msg.createdAt + ' good admin call from ' + msg.author.username);
-            var arg = msg.content.slice(7);
+			let arg = msg.content.slice(7);
 			//above removes the !admin call
 			//The following 'if' checks the next argument
 			if(arg.startsWith('playstatus')){
@@ -88,21 +91,20 @@ client.on('message', msg => {
 				console.log(chalk.yellow('<REPORT> ')+'status change call ');
 				if(arg.length == 0){
                     msg.reply('I need a new playing Status!');
-                    return;
                 }else{
                     client.user.setPresence({ game: { name: arg }});
                     console.log(chalk.red('<IMPORTANT> ') + 'The status was changed to "' + arg + '"');
 					console.log(chalk.yellow('<REPORT> ')+msg.createdAt + ' action performed by:' + msg.author.username);
-                    msg.reply('status changed.');
-                    return;
+                    msg.reply('status changed.').then();
+
 				}
             }else if(arg.startsWith('onlinestatus')){
                 arg = arg.slice(13);
-                if(arg.length ==0){
+                if(arg.length == 0){
                     msg.reply('I need an new online status! <online,dnd>');
                 }else{
-                    if(arg.toString() == "away"){
-                        msg.reply("I do not support an 'away' status, please use online or dnd.")
+                    if(arg.toString() === "away"){
+                        msg.reply("I do not support an 'away' status, please use online or dnd.").then()
                     
                     }else{
                         client.user.setPresence({ status: arg.toString() });
@@ -114,27 +116,28 @@ client.on('message', msg => {
 				msg.reply("I need a argument... \n USAGE - !admin <playstatus/onlinestatus/...>");
 			}
 		}
-	}else if(msg.content === 'gamer'){
+	}else if(msg.content.includes('gamer')){
 		//a fun little command with a 1/10th chance to respond to someone saying 'gamer'
 		var gamerRandom = Math.floor(Math.random() * 11);
 		console.log('rng was ' + gamerRandom);
-		if(gamerRandom == 10){
+		if(gamerRandom == 5){
 			msg.reply("We don't say the hard R here...");
 		}
-    }else if(msg.content === 'This poor bot.'){
-		//another fun thing, except it *should* also report RAM value... <- i dunno how to do it.
+    }else if(msg.content.includes("this poor bot")){
+		//another fun thing, except it *should* also report RAM value... //TODO Add RAM report.
         msg.reply("I feel bad for Contra's RAM lol.");
 		
     }else if(msg.content.startsWith('!UserPurge')){
         //This works to destroy a specific user's messages
+		//TODO fix TypeError from not tagging a user...
         var purgeUser = msg.mentions.users.first();
         var purgeUserID = purgeUser.id;
         console.log(purgeUserID);
 		var messageList = msg.channel.fetchMessages();
         msg.channel.fetchMessages().then(messages => {
-            var count = messages.filter(m => m.author.id === purgeUserID).size;
-			var purgeList = messages.filter(m => m.author.id === purgeUserID);
-            console.log('Attempting purge of '+count+' message(s)');
+			let count = messages.filter(m => m.author.id === purgeUserID).size;
+			let purgeList = messages.filter(m => m.author.id === purgeUserID);
+			console.log('Attempting purge of '+count+' message(s)');
             //This works on messages that are older than 14 days old
             //Discord API Limit on bulk API requests, however, this technically isn't bulk.
 			messages.forEach(m => { //recursion!
@@ -147,18 +150,18 @@ client.on('message', msg => {
 		msg.reply('check the console to assure objective completion.');
     }else if(msg.content.startsWith('!IDPurge')){
         //This works to destroy a specific user's messages, based on user ID
-        var purgeID = msg.content.slice(9, 27);
-        purgeID = purgeID.toString();
+		let purgeID = msg.content.slice(9, 27);
+		purgeID = purgeID.toString();
         console.log(purgeID);
-		var aroundID = msg.content.slice(28, 46);
+		let aroundID = msg.content.slice(28, 46);
 		aroundID = aroundID.toString();
 		if(aroundID == ''){
 			msg.reply('I need a message ID for deletion request.');
 		}else{
-			var messageIDList = msg.channel.fetchMessages();
+			let messageIDList = msg.channel.fetchMessages();
 			msg.channel.fetchMessages({ around: aroundID }).then(messages => {
-				var count = messages.filter(m => m.author.id === purgeID).size;
-				var purgeList = messages.filter(m => m.author.id === purgeID);
+				let count = messages.filter(m => m.author.id === purgeID).size;
+				let purgeList = messages.filter(m => m.author.id === purgeID);
 				console.log('Attempting purge of '+count+' message(s)');
 				//This works on messages that are older than 14 days old... Discord API Limit on bulk API requests, however, this technically isn't bulk.
 				messages.forEach(m => { //recursion!
@@ -176,13 +179,6 @@ client.on('message', msg => {
 			
 		}else{
 			msg.reply('im workin on it...');
-		}
-	}else if(msg.author.id == 154380183644798976){
-		const calloutChange = Math.floor(Math.random() * 8);
-		if(msg.channel.nsfw == true){
-			if(calloutChange == 10){
-				msg.channel.send('stop being horny!');
-			}
 		}
 	}else if(msg.content.startsWith('!horny')){
 		
@@ -204,9 +200,132 @@ client.on('message', msg => {
 	}else if(msg.content.startsWith('!gabby')){
 		
 		outstandingGirls("gabby");
-	}else if(msg.content.startsWith('fuck you')){
+	}else if(msg.content.includes('fuck you')){
 		msg.channel.send("fffFFUCK yOU");
+
 	}else if(msg.content.startsWith('!retro')){
 		msg.channel.send("the fuck do you want me to say??");
+
+	}else if(msg.content.includes('420') && msg.author.id != 669273008967516172){
+		msg.channel.send("no");
+
+	}else if(msg.content.startsWith('!430')){
+		msg.channel.send("430 > 420, obviously more dank");
+
+	}else if(msg.content.includes("beans")){
+		msg.channel.send("https://media.discordapp.net/attachments/660329145804390410/686041583405105182/image0.jpg?width=406&height=625");
+
+	}else if(msg.content.startsWith('!TKCommands')){
+		const embed = new RichEmbed();
+		embed.setTitle("Commands")
+			.setColor("#00ffff")
+			.setDescription('\n!TKInfo - Prints info about the bot \n\n' +
+				'!pog - fun little command. \n\n' +
+				'!admin - admin stuff \n\n' +
+				'gamer - fun little command to chew out people saying gamer \n\n' +
+				'!UserPurge - the main function of this bot... !helpPurge \n\n' +
+				"~MSGCount - command used for debugging/indexing of a user ID's message count \n\n" +
+				'!horny - call people out for being horny \n\n' +
+				'!announce - command to make the bot say something \n\n' +
+				'!catherine - certified good girl \n\n' +
+				'!aevery - certified good girl \n\n' +
+				'!gabby - certified good girl(?) \n\n' +
+				'!retro - the fuck you want me to say \n\n' +
+				"there's also like a dozen easter eggs...");
+		msg.channel.send(embed);
+
+	}else if(msg.content.includes("are you on drugs")){
+		msg.channel.send("lol yeah");
+
+	}else if(msg.content.includes("¯\\_(ツ)_/¯")){
+		msg.channel.send("make up your mind you twatmuffin.");
+
+	}else if(msg.content.includes("strings")){
+		msg.channel.send("https://cdn.discordapp.com/attachments/655642205465215016/703141130912399370/unknown.png");
+
+	}else if(msg.content.includes("is") && msg.content.includes("asleep")){
+		if(msg.content.includes("ember") || msg.content.includes("gabby")){
+			msg.channel.send("idk but they probably should be. https://cdn.discordapp.com/attachments/158125349769576448/700588892805660682/8af7e897e8d7beae83d188832adc8fb7.mp4")
+		}else if(msg.content.includes("poyo")){
+			msg.channel.send("poyo");
+		}else {
+			msg.channel.send("idk dude, go kiss them and find out");
+		}
+
+	}else if(msg.content.includes("cdn") && msg.author.id != 669273008967516172){
+		msg.channel.send("dis gon be gud");
+
+	}else if(msg.content.startsWith("!ember")){
+		outstandingGirls("ember");
+
+	}else if(msg.content.includes("what is scp-")){
+		let nameOfSCP = msg.content.slice(12);
+		nameOfSCP = parseInt(nameOfSCP);
+		if (nameOfSCP === 173) {
+			msg.channel.send("https://cdn.discordapp.com/attachments/572675846679166977/702064325019303946/dont_worry_about_it.png");
+		}else if(isNaN(nameOfSCP)){
+			msg.channel.send("lol nope");
+		} else if(nameOfSCP > 5099){
+			msg.channel.send("prolly doesn't exist, get lost");
+		}else {
+			msg.channel.send("http://www.scp-wiki.net/scp-" + nameOfSCP);
+		}
+
+	}else if(msg.content.includes("weeb") && msg.author.id != 669273008967516172){
+		msg.channel.send("nice, you fucking weeb");
+
+	}else if(msg.content.includes("shut up")){
+		let rando = Math.floor(Math.random()*2);
+		switch (rando) {
+			case 0:
+				msg.channel.send("https://i.redd.it/wrc2zc4rt0e31.jpg");
+			break;
+			case 1:
+				msg.channel.send("https://tenor.com/view/warframe-simaris-suck-my-data-gif-12190778");
+			break;
+			case 2:
+				msg.channel.send("https://images-na.ssl-images-amazon.com/images/I/514iO8ezBuL._AC_SL1001_.jpg");
+			break;
+		}
+
+	}else if(msg.content.includes("wait")){
+		msg.channel.send("oh no");
+
+	}else if(msg.content.includes("a toaster")){
+		msg.channel.send("https://cdn.discordapp.com/attachments/595436575177834496/703847959472636004/photo_2020-04-26_01-00-13.jpg");
+
+	}else if(msg.content.includes("cat ")){
+		msg.channel.send("https://cdn.discordapp.com/attachments/621695346371985408/702217363847774229/image0.jpg");
+
+	}else if(msg.content.startsWith("F")){
+		msg.channel.send("salute... o7");
+
+	}else if(msg.content.includes("uwu") && msg.author.id != 669273008967516172){
+		msg.channel.send(":uwu:");
+		
+	}else if(msg.content.startsWith('!roll')){
+		let dPos = msg.content.indexOf("d");
+		let arg1 = msg.content.slice(5,dPos);
+		let arg2 = msg.content.slice(dPos + 1);
+		if(isNaN(parseInt(arg1))){
+			msg.channel.send("please input a number for the number of die to be rolled, followed by a 'd'...");
+		}else if(isNaN(parseInt(arg2))){
+			msg.channel.send("please input a number for the number of sides on the die...");
+		} else{
+			msg.channel.send(diceRoll(arg1, arg2));
+		}
 	}
 });
+
+function diceRoll(numberOfDie, sizeOfDie){
+	var sum = 0;
+	let output = "The dice values are... ";
+	for(i = 0; i < numberOfDie; i++){
+		let dice = Math.floor((Math.random()*sizeOfDie)+1);
+		sum += dice;
+		output += dice;
+		output += ", ";
+	}
+	output += "and the sum is..." + sum;
+	return output;
+}
